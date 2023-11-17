3996d5c49ba4175cd3e890aa7a8ba694a7534cf4
Sherly Santiadi <71547739+sntdshrly@users.noreply.github.com>
2023-11-17 20:13:18 +0700

    Add files via upload

---
+++ uts4.py
@@ -0,0 +1,15 @@
+def divide(a, b):
+    result = 0
+    print(b!=0)
+    if b != 0:
+        result = a / b
+    return result
+
+def main():
+    print("Hasil :",divide(10,0))
+
+
+    return 0
+
+if __name__ == '__main__':
+    main()
